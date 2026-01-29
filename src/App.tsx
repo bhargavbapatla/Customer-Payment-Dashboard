af0a38b6cb57e4bc0c29a41a2c146e24ac76a01e
bhargavbapatla <bhargav.bapatla20@gmail.com>
2026-01-30 02:12:55 +0530

    removed placeholder image

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,6 @@ import DataTable from './components/table/page'
 function App() {
   return (
     <>
-      <div>
-        <h1 className="text-3xl font-bold underline mb-4">Payments Dashboard</h1>
-      </div>
       <div className="card space-y-4">
         <DataTable />
       </div>
